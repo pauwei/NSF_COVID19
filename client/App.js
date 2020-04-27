c18fe557198327ea31daa8bac098d7df6c818b02
essie-adm-qizhu <essie-adm-qizhu@ad.ufl.edu>
2020-04-27 10:02:23 -0400

    changed from hookform to formik

--- client/App.js
+++ client/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import AssetExample from './components/AssetExample.js'
+import {Login} from './components/Login'
+import BasicForm from './components/BasicForm'
 
 export default class App extends React.Component {
+  // TestApp()
   render(){
-    return <AssetExample />;
+    // return <AssetExample />;
+     return <BasicForm/>;
   }
 }
