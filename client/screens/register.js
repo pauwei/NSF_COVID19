7c8cf8a9adb6a61a4900d84ccd40d85bf7a41b6a
Paul Wei <pwei1@ufl.edu>
2020-04-30 16:34:19 -0400

    creating register page

---
+++ client/screens/register.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, TouchableHighlight, Image, Alert } from 'react-native';
+
+export default class Register extends React.Component {
+    
+}
