dc3b798d6635d2f3fb42be8fa370b49eb72c675c
unknown <zhoutianyu@ufl.edu>
2020-05-01 18:28:30 -0400

    Notification

--- client/App.js
+++ client/App.js
@@ -65,34 +65,34 @@ export default class PushNotification extends Component {
           _displayInForeground: true,        
         };
 
-        let not0 = new Date(2020, 4, 3, 10, 00, 00);
+        let not0 = new Date(2020, 4, 3, 10, 0, 0);
         not0 = Date.parse(not0);
-        let not1 = new Date(2020, 4, 4, 10, 00, 00);
+        let not1 = new Date(2020, 4, 4, 10, 0, 0);
         not1 = Date.parse(not1);
-        let not2 = new Date(2020, 4, 5, 10, 00, 00);
+        let not2 = new Date(2020, 4, 5, 10, 0, 0);
         not2 = Date.parse(not2);
-        let not3 = new Date(2020, 4, 6, 10, 00, 00);
+        let not3 = new Date(2020, 4, 6, 10, 0, 0);
         not3 = Date.parse(not3);
-        let not4 = new Date(2020, 4, 7, 10, 00, 00);
+        let not4 = new Date(2020, 4, 7, 10, 0, 0);
         not4 = Date.parse(not4);
-        let not5 = new Date(2020, 4, 8, 10, 00, 00);
+        let not5 = new Date(2020, 4, 8, 10, 0, 0);
         not5 = Date.parse(not5);
-        let not6 = new Date(2020, 4, 9, 10, 00, 00);
+        let not6 = new Date(2020, 4, 9, 10, 0, 0);
         not6 = Date.parse(not6);
 
-        let not10 = new Date(2020, 5, 3, 20, 00, 00);
+        let not10 = new Date(2020, 5, 3, 20, 0, 0);
         not10 = Date.parse(not10);
-        let not11 = new Date(2020, 5, 4, 20, 00, 00);
+        let not11 = new Date(2020, 5, 4, 20, 0, 0);
         not11 = Date.parse(not11);
-        let not12 = new Date(2020, 5, 5, 20, 00, 00);
+        let not12 = new Date(2020, 5, 5, 20, 0, 0);
         not12 = Date.parse(not12);
-        let not13 = new Date(2020, 5, 6, 20, 00, 00);
+        let not13 = new Date(2020, 5, 6, 20, 0, 0);
         not13 = Date.parse(not13);
-        let not14 = new Date(2020, 5, 7, 20, 00, 00);
+        let not14 = new Date(2020, 5, 7, 20, 0, 0);
         not14 = Date.parse(not14);
-        let not15 = new Date(2020, 5, 8, 20, 00, 00);
+        let not15 = new Date(2020, 5, 8, 20, 0, 0);
         not15 = Date.parse(not15);
-        let not16 = new Date(2020, 5, 9, 20, 00, 00);
+        let not16 = new Date(2020, 5, 9, 20, 0, 0);
         not16 = Date.parse(not16);
 
         const schedulingOptions0 = {
