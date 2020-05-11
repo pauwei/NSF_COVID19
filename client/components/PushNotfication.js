d08c813c70c53a09cf22669f92f06cd6c739aa16
unknown <zhoutianyu@ufl.edu>
2020-05-11 18:17:30 -0400

    notification change

--- client/components/PushNotfication.js
+++ client/components/PushNotfication.js
@@ -72,34 +72,34 @@ export default class PushNotification extends Component {
           _displayInForeground: true,        
         };
 
-        let not0 = new Date(2020, 4, 3, 10, 0, 0);
+        let not0 = new Date(2020, 4, 12, 10, 0, 0);
         not0 = Date.parse(not0);
-        let not1 = new Date(2020, 4, 4, 10, 0, 0);
+        let not1 = new Date(2020, 4, 13, 10, 0, 0);
         not1 = Date.parse(not1);
-        let not2 = new Date(2020, 4, 5, 10, 0, 0);
+        let not2 = new Date(2020, 4, 14, 10, 0, 0);
         not2 = Date.parse(not2);
-        let not3 = new Date(2020, 4, 6, 10, 0, 0);
+        let not3 = new Date(2020, 4, 15, 10, 0, 0);
         not3 = Date.parse(not3);
-        let not4 = new Date(2020, 4, 7, 10, 0, 0);
+        let not4 = new Date(2020, 4, 16, 10, 0, 0);
         not4 = Date.parse(not4);
-        let not5 = new Date(2020, 4, 8, 10, 0, 0);
+        let not5 = new Date(2020, 4, 10, 10, 0, 0);
         not5 = Date.parse(not5);
-        let not6 = new Date(2020, 4, 9, 10, 0, 0);
+        let not6 = new Date(2020, 4, 10, 10, 0, 0);
         not6 = Date.parse(not6);
 
-        let not10 = new Date(2020, 5, 3, 20, 0, 0);
+        let not10 = new Date(2020, 4, 12, 20, 0, 0);
         not10 = Date.parse(not10);
-        let not11 = new Date(2020, 5, 4, 20, 0, 0);
+        let not11 = new Date(2020, 4, 13, 20, 0, 0);
         not11 = Date.parse(not11);
-        let not12 = new Date(2020, 5, 5, 20, 0, 0);
+        let not12 = new Date(2020, 4, 14, 20, 0, 0);
         not12 = Date.parse(not12);
-        let not13 = new Date(2020, 5, 6, 20, 0, 0);
+        let not13 = new Date(2020, 4, 15, 20, 0, 0);
         not13 = Date.parse(not13);
-        let not14 = new Date(2020, 5, 7, 20, 0, 0);
+        let not14 = new Date(2020, 4, 16, 20, 0, 0);
         not14 = Date.parse(not14);
-        let not15 = new Date(2020, 5, 8, 20, 0, 0);
+        let not15 = new Date(2020, 4, 10, 20, 0, 0);
         not15 = Date.parse(not15);
-        let not16 = new Date(2020, 5, 9, 20, 0, 0);
+        let not16 = new Date(2020, 4, 10, 20, 0, 0);
         not16 = Date.parse(not16);
 
         const schedulingOptions0 = {
@@ -145,50 +145,76 @@ export default class PushNotification extends Component {
         const schedulingOptions16 = {
           time: not16,  
         };
-
+        if (not0 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions0
         );
+        }
+        if (not1 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions1
         );
+        }
+        if (not2 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions2
         );
+        }
+        if (not3 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions3
         );
+        }
+        if (not4 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions4
         );
+        }
+        if (not5 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions5
         );
+        }
+        if (not6 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions6
         );
-
+        }
+        if (not10 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions10
         );
+        }
+        if (not11 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions11
         );
+        }
+        if (not12 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions12
         );
+        }
+        if (not13 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions13
         );
+        }
+        if (not14 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions14
         );
+        }
+        if (not15 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions15
         );
+        }
+        if (not16 > (new Date()).getTime()) {
         Notifications.scheduleLocalNotificationAsync(
             localNotification0, schedulingOptions16
         );
+        }
 
         if (Constants.isDevice && result.status === 'granted') {
           //console.log('Notification permissions granted.')
