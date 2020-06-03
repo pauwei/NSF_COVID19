1868143eac408a2da2de482e008ea68ddd07ea3a
Tianyu Zhou <zhoutianyu@ufl.edu>
2020-06-03 18:24:34 -0400

    add rule

--- client/components/forms/CardComp.js
+++ client/components/forms/CardComp.js
@@ -21,7 +21,13 @@ const CardComp = () => (
       <Paragraph >Thank you for participating in the urban mobility survey. We will occasionally ask your opinions about outdoor trips in the next three months. 
         Your answers will help us better investigate urban mobility. Your identity and answers will be completely anonymous and for research only. 
         Data will not be shared with anybody or parties other than the research team at University of Florida. This research is supported by National Science Foundation (NSF). 
-        You will receive $30 gift card for the successful completion of the participation. </Paragraph>
+        You will receive $30 gift card for the successful completion of the participation. 
+        {'/n'}
+        In the next 90 days, you will receive 30 notifications to complete a short survey (<10 minuets). After completing each survey successfully, you will receive one token. 
+        After receiving 30 tokens, you will be contacted by us to receive the $30 Amazon Gift card via the registration email. If you have any question please contact us at icicvrufl@gmail.com
+        {'/n'}
+        Please note, Appe is not the sponsor of this survey study. This survey study is sponsored by National Science Foundation (NSF). All funds for Amazon Gift cards are provided by NSF.
+        </Paragraph>
     </Card.Content>
 
     <Card.Actions style={{  flex: 1,  }}>
